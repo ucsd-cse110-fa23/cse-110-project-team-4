c4f07410d80e941458a66e253b86f3b1add04703
rhoqan <114369881+rhoqan@users.noreply.github.com>
2023-11-04 00:46:55 -0700

    Sample Test

---
+++ src/client/test/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AppTest {
+    @Test
+    void sampleTest() {
+        assertEquals(4, 2*2);
+    }
+}
