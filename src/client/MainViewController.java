9d431d829164942dfad9c8d33c58f45cd904a74a
bosskaboss <dibyeshsahoo02@gmail.com>
2023-11-05 15:59:03 -0800

    Detailed View Window
    Also made the window of the app be controlled from main view controller

--- src/client/MainViewController.java
+++ src/client/MainViewController.java
@@ -7,14 +7,17 @@ import javafx.stage.Stage;
 public class MainViewController {
     Stage mpvcStage = new Stage();
     Stage drvStage = new Stage();
+    final int screenSizeWidth = 500;
+    final int screenSizeHeight = 750;
+
     MainViewController() {
         // Set the title of the app
 
-        MultipleRecipeViewController mpv = new MultipleRecipeViewController(mpvcStage);
-        DetailedViewController dvc = new DetailedViewController(drvStage);
-        dvc.display();
-
-        mpv.display();
+        MultipleRecipeViewController mpv = new MultipleRecipeViewController(mpvcStage,screenSizeWidth,screenSizeHeight);
+        DetailedViewController dvc = new DetailedViewController(drvStage,screenSizeWidth,screenSizeHeight);
         
+        
+        mpv.display();
+        dvc.display();
     }
 }
