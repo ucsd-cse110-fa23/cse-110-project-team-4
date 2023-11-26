b15952f0bb0a03347b568bd1119e69050c8036af
bosskaboss <dibyeshsahoo02@gmail.com>
2023-11-25 23:47:30 -0800

    did the UI Display of the url

--- src/client/FactoryForDetailedViews/FDRV1.java
+++ src/client/FactoryForDetailedViews/FDRV1.java
@@ -1,12 +1,12 @@
 package client.FactoryForDetailedViews;
 
 import client.DView;
-import client.DetailedRecipeViewTemplate;
+import client.DetailedOldRecipeView;
 import client.DetailedViewController;
 
 public class FDRV1 extends AbstractFactoryForDetailedView<DetailedViewController> {
     @Override
     public DView getView(DetailedViewController dvc) {
-        return new DetailedRecipeViewTemplate(dvc);
+        return new DetailedOldRecipeView(dvc);
     }  
 }
