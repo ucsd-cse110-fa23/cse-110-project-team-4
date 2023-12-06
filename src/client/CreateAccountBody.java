4c52d28b9f09022c97e807f3a30050951e20cbaf
Ryan Ho <114369881+rhoqan@users.noreply.github.com>
2023-12-05 21:51:50 -0800

    Automatic Login/Tests (#137)
    * automatic login tests
    
    * Update AutomaticLoginTest.java

--- src/client/CreateAccountBody.java
+++ src/client/CreateAccountBody.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
