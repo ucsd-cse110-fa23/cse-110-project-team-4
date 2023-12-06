c0bb6f361c527b7772ccce5317f8e697963f6f5b
Ryan Ho <114369881+rhoqan@users.noreply.github.com>
2023-12-05 23:17:38 -0800

    Update gitignore and Some Comments (#140)
    * Update .gitignore
    
    * Comments for generate recipe

--- src/client/generateRecipes/GenerateRecipesBody.java
+++ src/client/generateRecipes/GenerateRecipesBody.java
@@ -9,18 +9,12 @@ import javafx.geometry.Pos;
  * Class for the body of the Generate Recipes Window, containing the transcription of the voice recording.
  */
 public class GenerateRecipesBody extends VBox{
-    // private Image microphoneImage;
-    // private ImageView imageView;
     private Label transcription;
     private HBox body;
 
     ScrollPane sp;
 
     public GenerateRecipesBody() {
-        // imageView = new ImageView();
-
-        //microphoneImage = new Image("./client/images/SeekPng.com_microphone-icon-png_458366.png");
-        //imageView.setImage(microphoneImage);
 
         transcription = new Label("Transcribed Audio Will Go Here");
 
@@ -30,21 +24,8 @@ public class GenerateRecipesBody extends VBox{
         transcription.setMinHeight(200);
         transcription.setMinWidth(120);
 
-        // imageView.setFitHeight(200);
-        // imageView.setFitWidth(120);
-
-        // HBox hb1 = new HBox();
         VBox vb1 = new VBox();
-
-        // hb1.getChildren().add(imageView);
         vb1.getChildren().add(transcription);
-
-        // sp = new ScrollPane();
-        // sp.setContent(vb1);
-
-        
-        // hb1.setPadding(new Insets(10, 50, 10, 50));
-
 
         body = new HBox();
         body.getChildren().addAll(vb1);
